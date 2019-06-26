bb95154927924e4b3697f7cfe3879c0bdab2896b
harsh jain <harshjniitr@gmail.com>
2019-06-26 13:04:00 +0530

    Change peer container port to 7051

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -38,16 +38,16 @@ setGlobals() {
     if [ $PEER -eq 0 ]; then
       CORE_PEER_ADDRESS=peer0.org1.example.com:7051
     else
-      CORE_PEER_ADDRESS=peer1.org1.example.com:8051
+      CORE_PEER_ADDRESS=peer1.org1.example.com:7051
     fi
   elif [ $ORG == "org2" ]; then
     CORE_PEER_LOCALMSPID="Org2MSP"
     CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
     CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
     if [ $PEER -eq 0 ]; then
-      CORE_PEER_ADDRESS=peer0.org2.example.com:9051
+      CORE_PEER_ADDRESS=peer0.org2.example.com:7051
     else
-      CORE_PEER_ADDRESS=peer1.org2.example.com:10051
+      CORE_PEER_ADDRESS=peer1.org2.example.com:7051
     fi
 
   elif [ $ORG == "browser" ]; then
@@ -55,9 +55,9 @@ setGlobals() {
     CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_BROWSER_CA
     CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/browser.example.com/users/Admin@browser.example.com/msp
     if [ $PEER -eq 0 ]; then
-      CORE_PEER_ADDRESS=peer0.browser.example.com:11051
+      CORE_PEER_ADDRESS=peer0.browser.example.com:7051
     else
-      CORE_PEER_ADDRESS=peer1.browser.example.com:12051
+      CORE_PEER_ADDRESS=peer1.browser.example.com:7051
     fi
   else
     echo "================== ERROR !!! ORG Unknown =================="
