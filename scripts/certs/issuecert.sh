fed7f2c33141c8f4b8718b089f3b740109138142
harsh jain <harshjniitr@gmail.com>
2019-08-15 20:02:03 +0530

    Add OCSP URI to domain.com cert

--- scripts/certs/issuecert.sh
+++ scripts/certs/issuecert.sh
@@ -11,10 +11,10 @@ openssl req \
 
 cp ../sample.ext $DOMAIN.ext
 
-sed -i "s/domain.com/$DOMAINNAME/g" $DOMAIN.ext 
+sed -i "s/domain.com/$DOMAINNAME/g" $DOMAIN.ext
 
 openssl x509 -req -in $DOMAIN.csr -CA ../$CA/$CA.crt -CAkey ../$CA/$CA.key -CAcreateserial \
--out $DOMAIN.crt -days 1825 -sha256 -extfile $DOMAIN.ext
+-out $DOMAIN.crt -days 1825 -sha256 -extensions v3_req -extfile $DOMAIN.ext
 
 openssl rsa -in $DOMAIN.key -out rsa-$DOMAIN.key
 
